439690fff1db7ece3b04d0102f5ca7bf9a44fcbc
Denise Fernandez <dcbfernandez@gmail.com>
2014-06-15 04:32:50 -0400

    Web service for retrieving all HS Twitter accounts

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,24 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import services.HackerSchoolAPI
+import play.api.libs.ws.Response
+import scala.concurrent.Future
 
 object Application extends Controller {
 
+  implicit val context = scala.concurrent.ExecutionContext.Implicits.global
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def getAllTwitterAccounts = Action.async {
+
+    val hackerSchoolAccounts: Future[Response] = HackerSchoolAPI.scrapeAllTwitterAccounts()
+
+    hackerSchoolAccounts.map(response => Ok("WOOOOO " + HackerSchoolAPI.generateJSONResponse(response) ))
+
   }
 
 }
---
+++ app/services/HackerSchoolAPI.scala
@@ -0,0 +1,128 @@
+package services
+
+import play.api.libs.ws.{Response, WS}
+import scala.concurrent.Future
+import play.api.libs.ws
+import org.jsoup.nodes.{Element, Document}
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
+import scala.collection.mutable
+import play.api.libs.json.{JsValue, Writes, Json}
+import com.fasterxml.jackson.annotation.JsonValue
+import play.Play
+
+
+object HackerSchoolAPI {
+
+  implicit val context = scala.concurrent.ExecutionContext.Implicits.global
+
+  implicit val batchlingWrites = new Writes[Batchling] {
+    def writes(batchling: Batchling) = Json.obj(
+      "name" -> batchling.name,
+      "twitter" -> batchling.twitterHandle
+    )
+  }
+
+  implicit val batchWrites = new Writes[Batch] {
+    def writes(batch: Batch) = Json.obj(
+      "id" -> batch.id,
+      "name" -> batch.name,
+      "batchlings" -> batch.batchlings
+    )
+  }
+
+
+
+  def extractAuthToken(response: ws.Response): String = {
+    val body = response.body;
+    val doc = Jsoup.parse(body);
+    doc.select("[name=authenticity_token]").first().`val`()
+  }
+
+  def buildBatch(batchElement: Element): Batch = {
+    val batchName :String = batchElement.attr("id");
+    val batch : Batch = new Batch(batchName, batchName);
+    import scala.collection.JavaConversions._
+    val batchlings : Elements = batchElement.getElementsByTag("li");
+    for (currPerson: Element <- batchlings) {
+      batch.addBatchling(currPerson.select("div.name > a").text(), getTwitterHandle(currPerson));
+    }
+    return batch
+  }
+
+  def getTwitterHandle(batchling : Element) : String =  {
+    val twitterLink : Element = batchling.select("[href*=twitter.com]").first();
+    if (twitterLink != null) {
+      val twitterUrl : String = twitterLink.attr("href");
+      val twitterHandle : String = twitterUrl.substring(twitterUrl.lastIndexOf("/") + 1, twitterUrl.length());
+      return twitterHandle;
+    } else {
+      return null;
+    }
+  }
+
+  def scrapeAllTwitterAccounts() : Future[Response] = {
+
+    val response: Future[Response] = for {
+      loginPageResp <- WS.url("https://www.hackerschool.com/login").get()
+      loginPostResp <- WS.url("https://www.hackerschool.com/sessions")
+                        .withHeaders("Content-Type" -> "application/x-www-form-urlencoded",
+                                     "User-Agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36",
+                                     "Cookie" -> loginPageResp.cookies.toString()
+                         )
+                        .post("utf8=%E2%9C%93&authenticity_token=" + extractAuthToken(loginPageResp) + "&email=" + Play.application().configuration().getString("hs.username") + "&password=" + Play.application().configuration().getString("hs.password") + "&commit=Log+in")
+
+    } yield loginPostResp
+
+//    response onSuccess {
+//      case resp => {
+//
+//      }
+//    }
+//
+//   Json.parse("")
+    response
+  }
+
+  def generateJSONResponse(resp: Response) : JsValue = {
+    //take the response and map it to an object list via JSoup
+    var batchList: List[Batch] = List[Batch]()
+    val document : Document = Jsoup.parse(resp.body)
+    val batchContent = document.getElementById("batches")
+    import scala.collection.JavaConversions._
+    val allBatches = batchContent.children().select("ul")
+    for (batch <- allBatches) {
+    val b : Batch = buildBatch(batch)
+    batchList = batchList :+ b
+  }
+    //then convert to JSON and return it
+    val json = Json.toJson(batchList)
+    json
+  }
+
+  class Batchling(val name: String, val twitterHandle: String) {
+    override def toString = {
+      name + " - " + twitterHandle
+    }
+  }
+
+  class Batch(val id: String, val name: String) {
+
+    var batchlings: mutable.LinkedList[Batchling] = mutable.LinkedList[Batchling]()
+
+    def addBatchling(name: String, twitterHandle: String) {
+      batchlings = batchlings :+ new Batchling(name, twitterHandle)
+    }
+
+    override def toString = {
+      val sb : StringBuilder = new StringBuilder
+      sb.append("Batch id: " + id + "\n")
+      for (person <- batchlings) {
+        sb.append(person + "\n")
+      }
+      sb.toString()
+    }
+
+  }
+
+}
