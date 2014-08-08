6c01c2a130446549e2abee0c72e0d515631d9c25
Denise Fernandez <dcbfernandez@gmail.com>
2014-08-08 18:40:41 -0400

    Cleaned up the code base, now returning lists that the user is subscribed to

--- app/controllers/Twitter.scala
+++ app/controllers/Twitter.scala
@@ -1,11 +1,9 @@
 package controllers
 
 import play.api.mvc.{Controller, Action}
-import play.api.libs.ws.WS
-import scala.util.{Try, Failure, Success}
-import scala.concurrent.Future
-import play.api.libs.json.{Json, JsValue}
+import play.api.libs.json.Json
 import services.TwitterAPI
+import services.TwitterAPI.TwitterGroup
 
 
 object Twitter extends Controller {
@@ -13,38 +11,17 @@ object Twitter extends Controller {
   implicit val context = scala.concurrent.ExecutionContext.Implicits.global
 
 
-  def getAPIResult(token: String, apiCall: String):Future[Try[JsValue]] = {
-    WS.url(apiCall)
-      .withHeaders("Authorization" -> ("Bearer " + token))
-      .get().map(response =>
-      response.status match {
-        case 200 => Success(Json.parse(response.body))
-        case _ => Failure(new RuntimeException("Web service call failed: " + response.body))
-      })
-  }
-
-  def getTwitterLists(token: String, username: String):Future[Try[JsValue]] = {
-    getAPIResult(token, "https://api.twitter.com/1.1/lists/list.json?screen_name=" + username)
-  }
-
-  def getTwitterSubscriptions(token: String, username: String):Future[Try[JsValue]] = {
-    getAPIResult(token, "https://api.twitter.com/1.1/lists/subscriptions.json?cursor=-1&screen_name=" + username)
-  }
-
-
   def getAllLists(username: String) = Action.async { implicit request =>
     for {
       accessToken <- TwitterOAuth.getAccessToken
       lists <- TwitterAPI.getTwitterLists(accessToken, username)
-      subscriptions <- getTwitterSubscriptions(accessToken, username)
     } yield {
       lists match {
-        case Success(l) => Ok(Json.toJson(l))
-        case Failure(t)   => InternalServerError(t.getMessage)
+        case l:Seq[TwitterGroup]=> Ok(Json.toJson(l))
+        case t:Throwable => InternalServerError(t.getMessage)
       }
     }
   }
-
 
   def getIntersection(listId: String) = play.mvc.Results.TODO
 }
--- app/services/TwitterAPI.scala
+++ app/services/TwitterAPI.scala
@@ -1,64 +1,48 @@
 package services
 
-import play.api.mvc.{Controller, Action}
 import play.api.libs.ws.WS
-import scala.util.{Try, Failure, Success}
 import scala.concurrent.Future
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
-import scala.util.Success
-import scala.util.Failure
 
 
 object TwitterAPI {
 
   implicit val context = scala.concurrent.ExecutionContext.Implicits.global
-  case class TwitterGroup(id_str: String, name: String)
-  implicit val twitterGroupFmt = Json.format[TwitterGroup]
+  case class TwitterGroup(id: String, name: String)
 
-//  implicit val twitterGroupReads: Reads[TwitterGroup] = (
-//    (JsPath \ "id_str").read[String] and
-//    (JsPath \ "name").read[String]
-//    )(TwitterGroup.apply _)
-//
-//  implicit val twitterGroupWrites: Writes[TwitterGroup] = (
-//    (JsPath \ "id").write[String] and
-//    (JsPath \ "name").write[String]
-//    )(unlift(TwitterGroup.unapply))
+  implicit val twitterGroupReads: Reads[TwitterGroup] = (
+    (JsPath \ "id_str").read[String] and
+    (JsPath \ "name").read[String]
+    )(TwitterGroup.apply _)
+
+  implicit val twitterGroupWrites: Writes[TwitterGroup] = (
+    (JsPath \ "id").write[String] and
+    (JsPath \ "name").write[String]
+    )(unlift(TwitterGroup.unapply))
 
 
-  def getAPIResult(token: String, apiCall: String):Future[Try[String]] = {
+  private def getAPIResult(token: String, apiCall: String):Future[JsValue] = {
     WS.url(apiCall)
       .withHeaders("Authorization" -> ("Bearer " + token))
       .get().map(response =>
       response.status match {
-        case 200 => Success(response.body)
-        case _ => Failure(new RuntimeException("Web service call failed: " + response.body))
-      })
-  }
-
-  def convertFromJsValueToObject(json: Try[String]): Try[Seq[TwitterGroup]] = {
-    json match {
-      case Success(v) => {
-        val whatever = Json.fromJson[Seq[TwitterGroup]](Json.parse(v))
-        whatever match {
-          case s:JsSuccess[Seq[TwitterGroup]] => Success(s.get)
-          case JsError(err) => Failure(new RuntimeException("Error converting from JsValue to object: " + err))
-        }
+        case 200 => Json.parse(response.body)
+        case _ => throw new RuntimeException("Web service call failed: " + response.body)
       }
-      case Failure(t) => Failure(t)
-    }
+    )
   }
 
-  def getTwitterLists(token: String, username: String):Future[Try[Seq[TwitterGroup]]] = {
-    val result:Future[Try[String]] = getAPIResult(token, "https://api.twitter.com/1.1/lists/list.json?screen_name=" + username)
-
-    result.map( x => convertFromJsValueToObject(x))
+  private def convertFromListJsValueToObject(json: JsValue): Seq[TwitterGroup] = {
+    json.as[Seq[TwitterGroup]]
   }
 
-//  def getTwitterSubscriptions(token: String, username: String):Future[Try[JsValue]] = {
-//    getAPIResult(token, "https://api.twitter.com/1.1/lists/subscriptions.json?cursor=-1&screen_name=" + username)
-//  }
+
+  def getTwitterLists(token: String, username: String):Future[Seq[TwitterGroup]] = {
+    val result:Future[JsValue] = getAPIResult(token, "https://api.twitter.com/1.1/lists/list.json?screen_name=" + username)
+
+    result.map( x => convertFromListJsValueToObject(x))
+  }
 
 
   def getIntersection(listId: String) = play.mvc.Results.TODO
